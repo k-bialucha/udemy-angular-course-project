507eaed3e5720566d935c2876b74ffd9cabe17bf
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-01 10:14:36 +0200

    L48: Header structure updated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `
-    <div class="Header-spacing"></div>
-    <div class="Header">Header</div>
-  `,
+  templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {}
