07c44b0a69f4331c3a5ae66807b76aa6896f6a61
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-01 11:13:38 +0200

    L52: recipe items from the RecipeList looped in using ngFor

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -14,6 +14,11 @@ export class RecipeListComponent implements OnInit {
       'tasty recipe',
       'https://66.media.tumblr.com/5f2a764b3d3a30180243f6f8071a59a6/tumblr_oywfmxecrX1w466nko1_500.jpg'
     ),
+    new Recipe(
+      'Another Test Recipe',
+      'super tasty recipe',
+      'https://66.media.tumblr.com/5f2a764b3d3a30180243f6f8071a59a6/tumblr_oywfmxecrX1w466nko1_500.jpg'
+    ),
   ];
 
   constructor() {}
