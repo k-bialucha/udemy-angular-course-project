e6df27dbaf88a95a52860618021070be7c383111
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-02 16:12:52 +0200

    L84: simple routing implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+const routes: string[] = ['recipes', 'shopping-list'];
+
+const DEFAULT_ROUTE = routes[0];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-udemy-course-project';
+  title: string = 'angular-udemy-course-project';
+  route: string = DEFAULT_ROUTE;
+
+  changeRoute(routeName: string): void {
+    this.route = routeName;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input() currentRoute: string;
+  @Output('routeChanged') routeChangeEmitter: EventEmitter<
+    string
+  > = new EventEmitter();
+
+  changeRoute(event: Event) {
+    event.preventDefault();
+
+    const routeName = (<HTMLElement>event.target).id;
+    this.routeChangeEmitter.emit(routeName);
+  }
+}
