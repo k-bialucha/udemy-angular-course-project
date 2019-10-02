8eaa195844d14d3867436f5ceed134dabd8cac2d
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-02 18:54:47 +0200

    L88: data from the input elements handled in the ShoppingListEdit component

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
-  styleUrls: ['./shopping-list-edit.component.scss']
+  styleUrls: ['./shopping-list-edit.component.scss'],
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: true }) nameInput: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInput: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onAddItem() {
+    const name = this.nameInput.nativeElement.value;
+    const amount = this.amountInput.nativeElement.value;
+    console.warn('onAddItem:', name, amount);
   }
-
 }
