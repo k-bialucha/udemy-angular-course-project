a88a98c4e5c4ea68f29801e6f9ef69b9ba519e4a
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-02 18:39:50 +0200

    L86: recipe data displayed in the RecipeDetails component

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -21,7 +21,7 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'Another Test Recipe',
       'super tasty recipe',
-      'https://66.media.tumblr.com/5f2a764b3d3a30180243f6f8071a59a6/tumblr_oywfmxecrX1w466nko1_500.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/c/c5/Korean_food-Tasty_ramyeon-03.jpg'
     ),
   ];
 
