57b08aef8761aa6c141acae5b5d54f66779977d8
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-02 17:34:10 +0200

    L85: recipe data passed to the RecipeListItem component

--- src/app/recipe-book/recipe-list-item/recipe-list-item.component.ts
+++ src/app/recipe-book/recipe-list-item/recipe-list-item.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list-item',
   templateUrl: './recipe-list-item.component.html',
-  styleUrls: ['./recipe-list-item.component.scss']
+  styleUrls: ['./recipe-list-item.component.scss'],
 })
 export class RecipeListItemComponent implements OnInit {
+  @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
