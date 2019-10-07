796953809cf0d952f01bded2e19dfbd1e29de25e
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-07 17:20:35 +0200

    L164: route params retrieved in the RecipeEdit component

--- src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.scss']
+  styleUrls: ['./recipe-edit.component.scss'],
 })
 export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode: boolean = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params.id;
+      this.editMode = !!params.id;
+    });
   }
-
 }
