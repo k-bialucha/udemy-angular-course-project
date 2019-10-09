d7436dffe718d97161343a26ec190685898f2055
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-09 17:15:33 +0200

    L216: ShoppingListEdit reimplemented using TD forms

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,15 +1,9 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  ElementRef,
-  EventEmitter,
-  Output,
-} from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 
 import { ShoppingListService } from '../../services/shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -17,18 +11,21 @@ import { ShoppingListService } from '../../services/shopping-list.service';
   styleUrls: ['./shopping-list-edit.component.scss'],
 })
 export class ShoppingListEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: true }) nameInput: ElementRef;
-  @ViewChild('amountInput', { static: true }) amountInput: ElementRef;
+  // @ViewChild('form', { static: true }) form: NgForm;
 
   @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor(private shoppingListService: ShoppingListService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // this.form.valueChanges.subscribe(value => {
+    //   console.log('some changes', value);
+    // });
+  }
 
-  onAddItem() {
-    const name = this.nameInput.nativeElement.value;
-    const amount = this.amountInput.nativeElement.value;
+  onAddItem(form: NgForm) {
+    const name = form.value.name;
+    const amount = form.value.amount;
 
     const newIngredient = new Ingredient(name, amount);
     this.shoppingListService.addIngredient(newIngredient);
