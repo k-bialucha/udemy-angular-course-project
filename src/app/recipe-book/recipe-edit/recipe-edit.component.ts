1a7daf86aa203a9faa3b3af9835fcda8fcccf39d
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-10 11:26:12 +0200

    L228: handling FormArray added to RecipeEdit component

--- src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+
 import { RecipesService } from 'src/app/services/recipes.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -12,6 +14,10 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editMode: boolean = false;
   form: FormGroup;
+
+  get controls() {
+    return (this.form.get('ingredients') as FormArray).controls;
+  }
 
   constructor(
     private route: ActivatedRoute,
@@ -30,19 +36,31 @@ export class RecipeEditComponent implements OnInit {
   private initForm() {
     let recipeName = '',
       recipeImagePath = '',
-      recipeDescription = '';
+      recipeDescription = '',
+      recipeIngredients: Ingredient[] = [];
 
     if (this.editMode) {
       const recipe = this.recipesService.recipes[this.id];
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe.ingredients) recipeIngredients = recipe.ingredients;
     }
+
+    const ingredientsFormControls: FormGroup[] = recipeIngredients.map(
+      ingredient => {
+        return new FormGroup({
+          name: new FormControl(ingredient.name),
+          amount: new FormControl(ingredient.amount),
+        });
+      }
+    );
 
     this.form = new FormGroup({
       name: new FormControl(recipeName),
       imagePath: new FormControl(recipeImagePath),
       description: new FormControl(recipeDescription),
+      ingredients: new FormArray(ingredientsFormControls),
     });
   }
 
