cb95ab62db854539f1377be0ac2b5215cf02e5d8
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-10 19:20:26 +0200

    L231: option to add and update recipe implemented in the RecipesService

--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -1,15 +1,24 @@
 import { Recipe } from '../shared/recipe.model';
 
 import { createSchnitzel, createRamen } from '../shared/recipeCreators';
+import { Subject } from 'rxjs';
 
 export class RecipesService {
+  public recipesChanged: Subject<Recipe[]> = new Subject<Recipe[]>();
+
   private _recipes: Recipe[] = [createSchnitzel(), createRamen()];
 
-  // private _selectedRecipeId: number = 1;
-
-  // public get selectedRecipe(): Recipe {
-  //   return this.recipes[this._selectedRecipeId];
   public get recipes(): Recipe[] {
     return this._recipes.slice();
+  }
+
+  addRecipe(recipe: Recipe) {
+    this._recipes.push(recipe);
+    this.recipesChanged.next(this.recipes);
+  }
+
+  updateRecipe(index: number, recipe: Recipe) {
+    this._recipes[index] = recipe;
+    this.recipesChanged.next(this.recipes);
   }
 }
